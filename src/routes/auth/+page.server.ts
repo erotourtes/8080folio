aa2141066d52d35d08a0612ebc6988ea8a76bfd8
erotourtes <erotourtes@gmail.com>
2024-05-21 21:03:07 +0300

    refactor: extracted fetching logic

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -27,22 +27,13 @@ const updateUserData = async ({
     .auth()
     .verifyIdToken(jwtToken)
     .catch(() => null);
-
   if (!user) throw error(500, 'Can not verify your identity');
 
   const curUserRef = usersRef.doc(user.uid);
   const doc = await curUserRef.get();
   if (doc.exists) return;
 
-  const githubData: TGithubUser | null = await fetch('https://api.github.com/user', {
-    headers: { Authorization: `Bearer ${accessToken}` },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      console.error(err);
-      return null;
-    });
-
+  const githubData = await getGithubUser(accessToken);
   if (!githubData) throw error(500, 'Can not get github data');
 
   const { bio, blog, public_repos, total_private_repos, repos_url } = githubData;
@@ -55,7 +46,22 @@ const updateUserData = async ({
   };
   await curUserRef.set(firebaseUser);
 
-  const githubRepos: TGithubRepo[] | null = await fetch(repos_url, {
+  const githubRepos = await getGithubRepos({ accessToken, repos_url });
+  if (!githubRepos) throw error(500, 'Can not get github repos');
+
+  const repos: TFirebaseRepo[] = githubRepos.map((repo) => {
+    const { languages_url, collaborators_url, stargazers_count, created_at, html_url } = repo;
+    return { html_url, languages_url, collaborators_url, stargazers_count, created_at };
+  });
+
+  await reposRef.add({
+    userId: user.uid,
+    repos,
+  });
+};
+
+const getGithubUser = async (accessToken: string): Promise<TGithubUser | null> => {
+  return await fetch('https://api.github.com/user', {
     headers: { Authorization: `Bearer ${accessToken}` },
   })
     .then((res) => res.json())
@@ -63,22 +69,21 @@ const updateUserData = async ({
       console.error(err);
       return null;
     });
+};
 
-  if (!githubRepos) throw error(500, 'Can not get github repos');
-
-  const repos: TFirebaseRepo[] = githubRepos.map((repo) => {
-    const { languages_url, collaborators_url, stargazers_count, created_at, html_url } = repo;
-    return {
-      html_url,
-      languages_url,
-      collaborators_url,
-      stargazers_count,
-      created_at,
-    };
-  });
-
-  await reposRef.add({
-    userId: user.uid,
-    repos,
-  });
+const getGithubRepos = async ({
+  accessToken,
+  repos_url,
+}: {
+  accessToken: string;
+  repos_url: string;
+}): Promise<TGithubRepo[] | null> => {
+  return await fetch(repos_url, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  })
+    .then((res) => res.json())
+    .catch((err) => {
+      console.error(err);
+      return null;
+    });
 };
