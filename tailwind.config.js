dae48588d1b3d1ecd51d02c208854749ec7a5446
Max Siryk <67370189+erotourtes@users.noreply.github.com>
2024-05-22 22:35:56 +0300

    Feat/assets (#1)
    * feat: added color theme
    
    * feat: added fonts
    
    * fix: formatter
    
    * feat: default header fonts
    
    * fix: styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,45 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        source: ['Source Code Pro', 'monospace'],
+        researcher: ['Researcher', 'sans-serif'],
+      },
+      colors: {
+        bg: {
+          DEFAULT: 'rgb(var(--color-bg) / <alpha-value>)',
+          alt: 'rgb(var(--color-bg-alt) / <alpha-value>)',
+        },
+        primary: {
+          DEFAULT: 'rgb(var(--color-primary) / <alpha-value>)',
+          alt: 'rgb(var(--color-primary-alt) / <alpha-value>)',
+          on: 'rgb(var(--color-on-primary) / <alpha-value>)',
+        },
+        secondary: {
+          DEFAULT: 'rgb(var(--color-secondary) / <alpha-value>)',
+          alt: 'rgb(var(--color-secondary-alt) / <alpha-value>)',
+          on: 'rgb(var(--color-on-secondary) / <alpha-value>)',
+        },
+        green: {
+          DEFAULT: 'rgb(var(--color-green) / <alpha-value>)',
+          brigth: 'rgb(var(--color-green-bright) / <alpha-value>)',
+          dark: 'rgb(var(--color-green-dark) / <alpha-value>)',
+        },
+        accent: {
+          DEFAULT: 'rgb(var(--color-accent) / <alpha-value>)',
+        },
+        destructive: {
+          DEFAULT: 'rgb(var(--color-destructive) / <alpha-value>)',
+          dark: 'rgb(var(--color-destructive-dark) / <alpha-value>)',
+          on: 'rgb(var(--color-on-destructive) / <alpha-value>)',
+        },
+        muted: {
+          DEFAULT: 'rgb(var(--color-muted) / <alpha-value>)',
+          on: 'rgb(var(--color-on-muted) / <alpha-value>)',
+        },
+      },
+    },
   },
   plugins: [],
 };
