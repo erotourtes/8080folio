53dc9c36ac38a73cd21c64446698a68140861fcf
erotourtes <erotourtes@gmail.com>
2024-05-22 08:39:13 +0300

    fix: added permanent listener for user state change

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,16 +1,10 @@
-import { refreshToken } from '$lib/api/auth';
 import { firebaseAuth, firebaseSignIn, firebaseSignOut } from '$lib/firebase/firebase.client';
 import { type User } from 'firebase/auth';
 import { writable } from 'svelte/store';
 
 const createUserStore = () => {
   const { subscribe, set } = writable<User | null>(firebaseAuth.currentUser, () => {
-    const unsubscribe = firebaseAuth.onAuthStateChanged(async (user) => {
-      set(user);
-
-      const token = await user?.getIdToken();
-      await refreshToken(token);
-    });
+    const unsubscribe = firebaseAuth.onAuthStateChanged(set);
     return () => {
       unsubscribe();
     };
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
+  import { onDestroy } from 'svelte';
   import '../app.css';
+  import { refreshToken } from '../lib/api/auth';
+  import { firebaseAuth } from '../lib/firebase/firebase.client';
+
+  const unsubscribe = firebaseAuth.onIdTokenChanged(async (user) => {
+    const token = await user?.getIdToken();
+    await refreshToken(token);
+  });
+
+  onDestroy(() => {
+    unsubscribe();
+  });
 </script>
 
 <slot />
