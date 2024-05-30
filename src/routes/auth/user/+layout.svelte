8e7393efb463c78d73a2f57a56fb30543b0b984d
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-05-30 22:29:00 +0300

    Complete classes

--- src/routes/auth/user/+layout.svelte
+++ src/routes/auth/user/+layout.svelte
@@ -3,22 +3,30 @@
   import Footer from './components/Footer.svelte';
   import Header from './components/Header.svelte';
 
-  //let y;
+  let y;
   let innerWidth = 0;
   let innerHeight = 0;
+
+  function goTop() {
+    document.body.scrollIntoView();
+  }
 </script>
 
 <div
   class="container relative mx-auto flex min-h-screen w-full max-w-[1400px] flex-col text-sm sm:text-base"
 >
-  <div class="">
-    <button>
+  <div
+    class={'fixed bottom-0 z-[10] flex w-full p-10 duration-200 ' +
+      (y > 0 ? ' opacity-full pointer-events-auto' : ' pointer-events-none opacity-0')}
+  >
+    <button on:click={goTop} class="ml-auto rounded-full bg-slate-900 text-green-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center" >
       <i class="fa-solid fa-arrow-up" />
     </button>
   </div>
-  <Header />
+  {* eslint-disable-next-line *}
+  <Header y={y} />
   <slot />
   <Footer />
 </div>
 
-<!-- <svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth /> -->
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
