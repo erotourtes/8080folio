6b42a43031c8b9867f2755d03cd19fc602f027df
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-05-31 08:18:12 +0300

    Get repositories from firebase

--- src/routes/auth/user/+page.svelte
+++ src/routes/auth/user/+page.svelte
@@ -1,5 +1,44 @@
 <script lang="ts">
   import Main from './components/Main.svelte';
+  import { firebaseDb } from '../../../lib/firebase/firebase.client';
+  import { collection, getDocs } from 'firebase/firestore';
+
+  interface Repository {
+    user_uid: string;
+    html_url: string;
+    contributors: any[];
+    languages: Record<string, any>;
+    stargazers_count: number;
+  }
+
+  export let repos: Repository[] = [];
+  let loading: boolean = true;
+
+  // TO DO: Get data from firestore
+  async function fetchUserData(): Promise<Repository[]> {
+    let array: Repository[] = [];
+    const querySnapshot = await getDocs(collection(firebaseDb, 'repos'));
+    querySnapshot.forEach((doc) => {
+      array.push(doc.data() as Repository);
+    });
+
+    console.log(`ARRAY LENGTH: ${array.length}`);
+    console.log(array[1]);
+    console.log(typeof array);
+    return array;
+  }
+
+  async function loadData() {
+    repos = await fetchUserData();
+    console.log('Loaded repos:', repos);
+    loading = false;
+  }
+
+  loadData();
 </script>
 
-<Main />
+{#if loading}
+  <p>Loading...</p>
+{:else}
+  <Main {repos} />
+{/if}
---
+++ src/routes/auth/user/components/Main.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let repos;
+  console.log('repoArray', repos);
+</script>
+
+<h1>User</h1>
