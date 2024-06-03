036ed73221d31a09480332963a0108f08b968661
KseniiaHorai <117169759+KseniiaHorai@users.noreply.github.com>
2024-06-03 19:05:50 +0300

    Change sign-in page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -26,9 +26,34 @@
   }
 </script>
 
-<form method="post" action="?/signIn" bind:this={form} use:enhance>
-  <button type="button" on:click={handleSignIn}>Login with Github</button>
-  <input type="hidden" name={FORM_ACCESS_TOKEN_NAME} value={accessToken} />
-  <input type="hidden" name={FORM_JWT_TOKEN_NAME} value={jwtToken} />
-  <button type="button" on:click={handleSignOut}>Log out</button>
-</form>
+<div class="flex h-screen flex-col items-center justify-center">
+  <img
+    src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+    alt="GitHub Logo"
+    class="mb-8 h-64 w-64 rounded-full"
+  />
+  <form
+    method="post"
+    action="?/signIn"
+    bind:this={form}
+    use:enhance
+    class="flex flex-col space-y-6"
+  >
+    <button
+      type="button"
+      on:click={handleSignIn}
+      class="rounded-full bg-green-500 px-8 py-4 font-semibold text-white shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+    >
+      Login with Github
+    </button>
+    <input type="hidden" name={FORM_ACCESS_TOKEN_NAME} value={accessToken} />
+    <input type="hidden" name={FORM_JWT_TOKEN_NAME} value={jwtToken} />
+    <button
+      type="button"
+      on:click={handleSignOut}
+      class="rounded-full bg-white px-8 py-4 font-semibold text-gray-800 shadow-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75"
+    >
+      Log out
+    </button>
+  </form>
+</div>
